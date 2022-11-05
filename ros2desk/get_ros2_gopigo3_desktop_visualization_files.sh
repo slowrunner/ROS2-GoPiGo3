b44ac21f2ff446d3768d391755c96de643ea9ed7
slowrunner <slowrunner@users.noreply.github.com>
2022-11-05 12:53:09 -0400

    Update get_ros2_gopigo3_desktop_visualization_files.sh

--- ros2desk/get_ros2_gopigo3_desktop_visualization_files.sh
+++ ros2desk/get_ros2_gopigo3_desktop_visualization_files.sh
@@ -3,6 +3,7 @@ echo -e "\n*** Downloading ROS2 GoPiGo3 Desktop Visualization Files"
 wget https://raw.githubusercontent.com/slowrunner/ROS2-GoPiGo3/main/ros2desk/dave.urdf
 wget https://raw.githubusercontent.com/slowrunner/ROS2-GoPiGo3/main/ros2desk/finmark.urdf
 wget https://raw.githubusercontent.com/slowrunner/ROS2-GoPiGo3/main/ros2desk/gpgMin.urdf
-wget show_rqt_graph.sh
-wget run_image_view.sh
-wget view_frames.sh
+wget https://raw.githubusercontent.com/slowrunner/ROS2-GoPiGo3/main/ros2desk/show_rqt_graph.sh
+wget https://raw.githubusercontent.com/slowrunner/ROS2-GoPiGo3/main/ros2desk/run_image_view.sh
+wget https://raw.githubusercontent.com/slowrunner/ROS2-GoPiGo3/main/ros2desk/view_frames.sh
+chmod +x *.sh
