cb600c2151980652fa12a38bba79e41d8d3d6e7d
slowrunner <slowrunner@users.noreply.github.com>
2022-11-05 12:47:09 -0400

    Create get_ros2_gopigo3_desktop_visualization_files.sh

---
+++ ros2desk/get_ros2_gopigo3_desktop_visualization_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -e "\n*** Downloading ROS2 GoPiGo3 Desktop Visualization Files"
+wget dave.urdf
+wget finmark.urdf
+wget gpgMin.urdf
+wget show_rqt_graph.sh
+wget run_image_view.sh
+wget view_frames.sh
