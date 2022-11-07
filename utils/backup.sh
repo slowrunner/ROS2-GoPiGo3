3a5fd852a74b74e62891421338ca127985034c40
slowrunner <slowrunner@users.noreply.github.com>
2022-11-07 13:23:25 -0500

    Create backup.sh

---
+++ utils/backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo -e "Compressing ros2ws, utils to /home/ubuntu/ROS2HH_Backup.tgz"
+cd ~/
+rm -rf ~/ros2ws/log/*
+rm -rf ~/ros3ws/roslogs/*
+tar -cvzf ROS2HH_Backup.tgz ros2ws utils systests myYDLidar-SDK test_motors.sh test_communication.sh
