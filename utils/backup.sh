723cd3b6b8cdb5a8c4aca44dbd0ae63785e1b123
slowrunner <slowrunner@users.noreply.github.com>
2022-11-28 11:09:26 -0500

    updates and restore tests at top level

--- utils/backup.sh
+++ utils/backup.sh
@@ -4,4 +4,4 @@ echo -e "Compressing ros2ws, utils to /home/ubuntu/ROS2HH_Backup.tgz"
 cd ~/
 rm -rf ~/ros2ws/log/*
 rm -rf ~/ros3ws/roslogs/*
-tar -cvzf ROS2HH_Backup.tgz ros2ws utils systests myYDLidar-SDK
+tar -cvzf ROS2HH_Backup.tgz Docs Graphics README.md myYDLidar-SDK ros2desk ros2ws systests test_communication.sh test_motors.sh updates utils
