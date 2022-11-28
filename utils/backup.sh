c5ecf37c51f8490854b1d776d3d0068317bbaecf
slowrunner <slowrunner@users.noreply.github.com>
2022-11-28 10:51:28 -0500

    as of 11-28-2022

--- utils/backup.sh
+++ utils/backup.sh
@@ -4,4 +4,4 @@ echo -e "Compressing ros2ws, utils to /home/ubuntu/ROS2HH_Backup.tgz"
 cd ~/
 rm -rf ~/ros2ws/log/*
 rm -rf ~/ros3ws/roslogs/*
-tar -cvzf ROS2HH_Backup.tgz ros2ws utils systests myYDLidar-SDK test_motors.sh test_communication.sh
+tar -cvzf ROS2HH_Backup.tgz ros2ws utils systests myYDLidar-SDK
